a3a65ec2b08b1937a59a6f2477dc556704ff2f08
Srijansarkar17 <srijanb463@gmail.com>
2025-06-30 13:35:55 +0530

    Learned Fragments to return many elements together

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,8 +2,13 @@ import { useState } from 'react'
 import Chai from './chai'
 
 function App() {
-  return (
+  return ( //<> and </> are called fragments and to pass many html elements we use this
+    <>
     <Chai />
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
+    
   )
 }
 
