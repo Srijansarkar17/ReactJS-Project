52697219d0be970d998a9b5d1cd0f71bae238c84
Srijansarkar17 <srijanb463@gmail.com>
2025-07-01 17:21:57 +0530

    Created an element using the createElement feature of React

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 
 import App from './App.jsx'
 
+
+const reactElement = React.createElement( //written the description in ipad
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
