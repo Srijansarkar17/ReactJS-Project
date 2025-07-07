33b430fdcdc0ca97797a1110058b786d0b947584
Srijansarkar17 <srijanb463@gmail.com>
2025-07-07 18:11:58 +0530

    set the useState variables initially
    the variables that are being used

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setnumberAllowed] = useState(false)
+  const [charsAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
   return (
     <>
