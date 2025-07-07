eaa6eabe5aab153cf01ac56d79d141b8c0fd8bcf
Srijansarkar17 <srijanb463@gmail.com>
2025-07-07 17:16:05 +0530

    setup the basic layout, functionality not added till now

--- bgChangerProject/src/App.jsx
+++ bgChangerProject/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color}}
+      >
+
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
+            <button className="outline-none px-4 py-1 rounded-2xl text-white shadow-lg"
+            style={{backgroundColor: "Red"}}
+            >Red</button>
+            <button className="outline-none px-4 py-1 rounded-2xl text-white shadow-lg"
+            style={{backgroundColor: "Green"}}
+            >Green</button>
+            <button className="outline-none px-4 py-1 rounded-2xl text-white shadow-lg"
+            style={{backgroundColor: "Blue"}}
+            >Blue</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
-}
-
+} 
+ //ClassName explainations : fixed: This makes the box stick to the screen no matter how you scroll. Like a sticker stuck on your monitor, flex: This tells the box to arrange things in a row, like people standing in a line, flex-wrap: If there’s not enough space, the items inside will move to the next line instead of being squished. Like wrapping presents when one doesn't fit
 export default App
