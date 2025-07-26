ea6037efe889831d465264cdbaf0353d6676b403
Srijansarkar17 <srijanb463@gmail.com>
2025-07-27 01:23:56 +0530

    started with app.jsx

--- 06currencyProject/src/App.jsx
+++ 06currencyProject/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {inputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo) //it takes out the keys from the currencyInfo object
 
   return (
     <>
